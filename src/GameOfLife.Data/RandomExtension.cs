b17cab9c3e615b541f6421f423075becf94c8179
foteliasz <wojciech.tomaszek@gmail.com>
2023-09-19 02:14:39 +0200

    feat: map randomization

---
+++ src/GameOfLife.Data/RandomExtension.cs
@@ -0,0 +1,9 @@
+﻿namespace GameOfLife.Data;
+
+public static class RandomExtension
+{
+    public static bool NextBoolean(this Random random, float? ratio = null)
+    {
+        return random.NextSingle() <= (ratio ?? 0.5f);
+    }
+}
---
+++ src/GameOfLife.Data/World.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace GameOfLife.Data;
+
+public class World
+{
+    private bool[,] _matrix;
+    private Random _seeded;
+    
+    public World(int rows, int columns)
+    {
+        _seeded = new Random();
+        _matrix = new bool[rows, columns];
+        Rows = rows;
+        Columns = columns;
+    }
+
+    public int Columns { get; }
+    public int Rows { get; }
+
+    public void Randomize(float? ratio = null)
+    {
+        for (var row = 0; row < Rows; row++)
+        {
+            for (var column = 0; column < Columns; column++)
+            {
+                _matrix[row, column] = _seeded.NextBoolean(ratio);
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        var writer = new StringBuilder();
+        for (var row = 0; row < Rows; row++)
+        {
+            for (var column = 0; column < Columns; column++)
+            {
+                writer.Append(_matrix[row, column]
+                    ? "■"
+                    : " ");
+            }
+
+            writer.AppendLine();
+        }
+
+        return writer.ToString();
+    }
+}
